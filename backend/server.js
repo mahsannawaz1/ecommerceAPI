99a9f4e908f04ad6f2984c08b06dce789169d7ad
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-06 05:00:15 -0700

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,13 @@ const mongoose = require('mongoose')
 winston.add(new winston.transports.Console())
 const app = express()
 
+mongoose.connect('mongodb://localhost/ecommerce')
+.then(()=>winston.info('Connected to MongoDB'))
+.catch(error=>winston.error(error.message,error))
+
+
+
+
 const port = process.env.PORT || 3000
 app.listen(port,()=>winston.info(`Listening at PORT: ${port}`))
 
