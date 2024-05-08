b27017ebbd4937ab9c2075972bbc22846afffc84
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-08 05:11:51 -0700

    add get request for category model

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -3,3 +3,7 @@ const Category = require('../models/Category')
 const Joi = require('joi')
 Joi.objectId = require('joi-objectid')(Joi)
 const router = require('express').Router()
+
+router.get('/',async(req,res)=>{
+    res.send(await Category.find())
+})
