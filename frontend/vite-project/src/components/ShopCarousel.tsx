540550b3c925259e0495ded8c4fab69857547dcb
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-18 13:32:52 -0700

    fixed bugs in the carousel

--- frontend/vite-project/src/components/ShopCarousel.tsx
+++ frontend/vite-project/src/components/ShopCarousel.tsx
@@ -7,7 +7,7 @@ import girlsImg from '../../public/girls.webp'
 import shirts from '../../public/shirts.webp'
 import menWomen from '../../public/men_women.webp'
 import americanEagle from '../../public/american_eagle.webp'
-import { Box, IconButton, Stack } from '@mui/material'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
 interface Props {
     type: 'PREV' | 'NEXT';
     onClick: () => void;
@@ -26,33 +26,39 @@ const ShopCarousel = () => {
         
 
     return (
-            <Box paddingX={1}>
-                <Carousel pagination={false} renderArrow={({type, onClick, isEdge}:Props)=>type=='PREV' ? <IconButton disabled={isEdge} disableRipple sx={{cursor:'pointer',visibility:isEdge ? 'hidden' : 'visible'}} onClick={onClick}><ArrowBackIosNewIcon  /> </IconButton> : <IconButton disabled={isEdge} sx={{cursor:'pointer',visibility:isEdge ? 'hidden' : 'visible'}} disableRipple onClick={onClick}> <ArrowForwardIosIcon /></IconButton> } breakPoints={breakpoints}>
+        <Stack>
+            <Typography fontWeight={600} variant='h5' textAlign={'center'}>Get Inspired!</Typography>
+                <Typography variant='subtitle1' textAlign={'center'}>Shop your favorite look! Mention @ahsan&usama's and you could be featured right here!</Typography>
+            <Box paddingX={1} marginY={4}>
+                <Carousel pagination={false}
+                renderArrow={({type, onClick, isEdge}:Props)=>type=='PREV' ? <IconButton disabled={isEdge} disableRipple sx={{cursor:'pointer',visibility:isEdge ? 'hidden' : 'visible'}} onClick={onClick}><ArrowBackIosNewIcon  /> </IconButton> : <IconButton disabled={isEdge} sx={{cursor:'pointer',visibility:isEdge ? 'hidden' : 'visible'}} disableRipple onClick={onClick}> <ArrowForwardIosIcon /></IconButton> } 
+                breakPoints={breakpoints}>
                 <Box 
+                
                 position={'relative'} width={{base:'300px',lg:'350px'}}>
-                    <video width='100%'   height={'400px'} style={{objectFit:'cover'}} src={video} />
+                    <video width='100%'   height={'350px'} style={{objectFit:'cover'}} src={video} />
                     <Box position={'absolute'} top='5px' right='5px'>
                         <IconButton disableRipple>
                             <PlayCircleOutlineIcon />
                         </IconButton>
                     </Box>
                 </Box>
-                <Box width={{base:'300px',lg:'350px'}}  marginX={1}>
-                    <img width='100%'  height={'400px'} style={{objectFit:'cover'}} src={girlsImg} />
+                <Box width={{base:'300px',lg:'350px'}}  marginX={2}>
+                    <img width='100%'  height={'350px'} style={{objectFit:'cover'}} src={girlsImg} />
                 </Box>
                 <Box width={{base:'300px',lg:'350px'}} >
-                    <img width='100%' height={'400px'} style={{objectFit:'cover'}} src={shirts} />
+                    <img width='100%' height={'350px'} style={{objectFit:'cover'}} src={shirts} />
                 </Box>
-                <Box width={{base:'300px',lg:'350px'}}  marginX={1}>
-                    <img width='100%'  height={'400px'} style={{objectFit:'cover'}} src={americanEagle} />
+                <Box width={{base:'300px',lg:'350px'}}  marginX={2}>
+                    <img width='100%'  height={'350px'} style={{objectFit:'cover'}} src={americanEagle} />
                 </Box>
                 <Box width={{base:'300px',lg:'350px'}} >
-                    <img width='100%' height={'400px'} style={{objectFit:'cover'}} src={menWomen} />
+                    <img width='100%' height={'350px'} style={{objectFit:'cover'}} src={menWomen} />
                 </Box>
                         </Carousel>
             </Box>
+        </Stack>
             
-         
     )
 }
 
