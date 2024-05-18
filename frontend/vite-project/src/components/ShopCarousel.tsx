63a797f20f2e58eb3360be37a59e629396032b47
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-18 15:11:11 -0700

    Add modal design

--- frontend/vite-project/src/components/ShopCarousel.tsx
+++ frontend/vite-project/src/components/ShopCarousel.tsx
@@ -3,21 +3,38 @@ import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+import CloseIcon from '@mui/icons-material/Close'
 import video from '../../public/denim_vid.mp4'
 import girlsImg from '../../public/girls.webp'
 import shirts from '../../public/shirts.webp'
 import menWomen from '../../public/men_women.webp'
 import americanEagle from '../../public/american_eagle.webp'
-import { Box, IconButton, Stack, Typography } from '@mui/material'
+import { Box, Button, Grid, IconButton, Modal, Stack, Typography } from '@mui/material'
+import { useState } from 'react'
 
 interface Props {
     type: 'PREV' | 'NEXT';
     onClick: () => void;
     isEdge: boolean;
-    }
+}
 
+const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+};
 
 const ShopCarousel = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
     const breakpoints :ReactElasticCarouselProps['breakPoints'] = [
         { width:1, itemsToShow:1, itemsToScroll:1 },
         { width:600, itemsToShow:2, itemsToScroll:1},
@@ -43,7 +60,7 @@ const ShopCarousel = () => {
                                 <PlayCircleOutlineIcon />
                             </IconButton>
                         </Box>
-                        <Stack position={'absolute'} top='0' direction={'column'} alignItems={'center'} justifyContent={'center'} width="100%" height='350px' maxHeight={"100%"} sx={{
+                        <Stack onClick={handleOpen} position={'absolute'} top='0' direction={'column'} alignItems={'center'} justifyContent={'center'} width="100%" height='350px' maxHeight={"100%"} sx={{
                             bgcolor:'rgb(0,0,0,0.8)',
                             color:'var(--white)',
                             cursor:'pointer',
@@ -153,6 +170,126 @@ const ShopCarousel = () => {
                     </Box>
                 </Carousel>
             </Box>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+        >
+        <Box  sx={{ ...style }}>
+            
+            <Stack direction='row'>
+            <video muted controls autoPlay width='400px'  style={{objectFit:'cover'}} src={video} />
+            <Box padding={2.5} maxWidth={230} maxHeight={'450px'} sx={{
+                overflowX:'hidden',
+                overflowY:'scroll'
+            }}>
+            
+                <Stack direction='row'>
+                    <Typography marginRight={3} alignSelf={'center'} variant='h5'>AU</Typography>
+                    <Typography alignSelf={'center'}>@ahsan&usama</Typography>
+                    <Box alignSelf={'flex-start'} sx={{
+                        transform:'translate(42px,-20px)'
+                    }}>
+                        <IconButton onClick={handleClose} sx={{
+                            color:'var(--black)',
+                        }} disableRipple>
+                            <CloseIcon fontSize='large' />
+                        </IconButton>
+                    </Box>
+                </Stack>
+                <Typography alignSelf={'center'}>Get Inspired! (3 items)</Typography>
+                <Grid container>
+                        <Grid item xs={6}>
+                            <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft='5px' sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft='5px' sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft={'5px'} sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft='5px' sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft='5px' sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Box width='100px' sx={{
+                                ':hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }}>
+                                <img src={menWomen}   height='100px' />
+                                <Typography variant='caption'>American Eagle Super Soft Thermal Shirt</Typography>
+                                <Typography variant='subtitle2'  color='error'>3500.00 
+                                    <Typography variant='subtitle2' component='span' marginLeft='5px' sx={{textDecoration:'line-through',color:'var(--black)'}}>5000.00 </Typography>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                </Grid>
+            </Box>
+            </Stack>
+        </Box>
+        </Modal>
         </Stack>
             
     )
