4e8c5451364561d090b35e3fd6b9469aceac07a0
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-18 16:20:55 -0700

    remove bug from Carousal component

--- frontend/vite-project/src/components/ShopCarousel.tsx
+++ frontend/vite-project/src/components/ShopCarousel.tsx
@@ -1,4 +1,3 @@
-import Carousel, { ReactElasticCarouselProps } from 'react-elastic-carousel'
 import video from '../../public/denim_vid.mp4'
 import girlsImg from '../../public/girls.webp'
 import shirts from '../../public/shirts.webp'
@@ -7,6 +6,8 @@ import americanEagle from '../../public/american_eagle.webp'
 import { Box, Stack, Typography } from '@mui/material'
 import ShopCarouselItem from './ShopCarouselitem'
 import CustomCarouselArrow from './CustomCarouselArrow'
+import Carousal from "@itseasy21/react-elastic-carousel";
+
 
 
 export interface Props {
@@ -16,7 +17,7 @@ export interface Props {
 }
 
 const ShopCarousel = () => {
-    const breakpoints :ReactElasticCarouselProps['breakPoints'] = [
+    const breakpoints = [
         { width:1, itemsToShow:1, itemsToScroll:1 },
         { width:600, itemsToShow:2, itemsToScroll:1},
         { width:900, itemsToShow:3, itemsToScroll:1 },
@@ -30,7 +31,7 @@ const ShopCarousel = () => {
             <Typography fontWeight={600} variant='h5' textAlign={'center'}>Get Inspired!</Typography>
             <Typography variant='subtitle1' textAlign={'center'}>Shop your favorite look! Mention @ahsan&usama's and you could be featured right here!</Typography>
             <Box paddingX={1} marginY={4}>
-                <Carousel pagination={false}
+                <Carousal isRTL={false} pagination={false}
                     renderArrow={({type, onClick, isEdge}:Props)=><CustomCarouselArrow type={type} onClick={onClick} isEdge={isEdge}  />} 
                     breakPoints={breakpoints}>
                         <ShopCarouselItem type='video' url={video} />
@@ -38,10 +39,9 @@ const ShopCarousel = () => {
                         <ShopCarouselItem type='image' url={shirts} />
                         <ShopCarouselItem type='image' url={americanEagle} />
                         <ShopCarouselItem type='image' url={menWomen} />
-                </Carousel>
+                </Carousal>
             </Box>
         </Stack>
-            
     )
 }
 
