02be262d9f9d71a4f556e1063ffaf531608ad173
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-23 06:50:13 -0700

    fixed layout of product list page

--- frontend/vite-project/src/components/ProductListGrid.tsx
+++ frontend/vite-project/src/components/ProductListGrid.tsx
@@ -20,31 +20,32 @@ const ProductListGrid = () => {
 
     return (
         <Grid container spacing={2}>
-            {productImages.map((image,index)=><Grid key={index} item>
-                <Box position='relative' width={250} height={300} sx={{cursor:'pointer'}}>
-                    <img  src={image} height='100%' width='100%'  />
-                    <IconButton  onClick={()=>handleChangeFavourites(index)} sx={{position:'absolute',right:'0px',color:'var(--black)'}} disableRipple> {foundFavourite(index) == index ? <FavoriteIcon /> : <FavoriteBorderIcon /> }  </IconButton>
-                </Box>
-                
-                <Box>
-                    <Typography fontSize={'var(--pro-title-font)'} sx={
-                        {
-                            '&:hover':{
-                                textDecoration:'underline',
-                                cursor:'pointer'
-                            }
-                        }
-                        } variant='body2'>AE LOGO GRAPHIC TANK TOP</Typography>
-                    <Box>
-                        <Typography variant='body2' sx={{ textDecoration:'line-through' }} component={'span'}>PKR 1,200</Typography>
-                        <Typography variant='body2' color='error' marginLeft={'5px'} component={'span'}>PKR 1,200</Typography>
+            {productImages.map((image,index)=>
+                <Grid key={index} item lg={3} md={4} sm={6} xs={12} >
+                    <Box position='relative' sx={{cursor:'pointer'}}>
+                        <img  src={image} height='100%' width='100%'  />
+                        <IconButton  onClick={()=>handleChangeFavourites(index)} sx={{position:'absolute',right:'0px',color:'var(--black)'}} disableRipple> {foundFavourite(index) == index ? <FavoriteIcon /> : <FavoriteBorderIcon /> }  </IconButton>
                     </Box>
                     
-                    <Typography variant='caption' color='error'>(Save 40%)</Typography>
-                </Box>
-                <Stack >
-                    <Button color='secondary'>View Options</Button>
-                </Stack>
+                    <Box>
+                        <Typography fontSize={'var(--pro-title-font)'} sx={
+                            {
+                                '&:hover':{
+                                    textDecoration:'underline',
+                                    cursor:'pointer'
+                                }
+                            }
+                            } variant='body2'>AE LOGO GRAPHIC TANK TOP</Typography>
+                        <Box>
+                            <Typography variant='body2' sx={{ textDecoration:'line-through' }} component={'span'}>PKR 1,200</Typography>
+                            <Typography variant='body2' color='error' marginLeft={'5px'} component={'span'}>PKR 1,200</Typography>
+                        </Box>
+                        
+                        <Typography variant='caption' color='error'>(Save 40%)</Typography>
+                    </Box>
+                    <Stack >
+                        <Button color='secondary'>View Options</Button>
+                    </Stack>
             </Grid>)}
             
         </Grid>
--- frontend/vite-project/src/components/ProductListPage.tsx
+++ frontend/vite-project/src/components/ProductListPage.tsx
@@ -1,4 +1,4 @@
-import { Stack,Grid,Box, Typography, Button} from '@mui/material'
+import { Stack,Grid,Box, Typography, Button, Container} from '@mui/material'
 import newArrivals from '../../public/newArrivals.png'
 import tees from '../../public/teesAvatar.png'
 import hoodies from '../../public/hoodiesAvatar.png'
@@ -32,160 +32,160 @@ const ProductListPage = () => {
         setFilters([...filters,obj])
     }
     return (
-        <Stack>
-        <Stack direction='row'  spacing={2}>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={newArrivals} alt="New Arrivals" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>New Arrivals</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={tees} alt="Graphic tees" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Graphic tees</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={polos} alt="Polos" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Polos</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={shirts} alt="Shirts & Flannels" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Shirts & Flannels</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={hoodies} alt="Hoodies & SweatShirts" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Hoodies & SweatShirts</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={jeans} alt="Jeans" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Jeans</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={joggers} alt="Joggers & Sweatpants" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Joggers & Sweatpants</Typography>
-            </Box>
-            <Box  sx={{ cursor:'pointer' }}>
-                <img width={'120px'}  src={under} alt="Underwear" />
-                <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Underwear</Typography>
-            </Box>
-        </Stack>
-        <Stack spacing={1} direction='row' justifyContent={'flex-start'} alignItems={'center'}>
-            <Typography variant='caption' sx={{
-                    color:'var(--link)',
-                    '&:hover':{
-                        
-                        textDecoration:'underline'
-
-                    },
-                    cursor:'pointer'
-                }}>Home </Typography>
-            <Typography variant='caption' sx={{
-                    color:'var(--link)',
-                    '&:hover':{
-                        
-                        textDecoration:'underline'
-
-                    },
-                    cursor:'pointer'
-                }}>&gt;</Typography>
-            <Typography variant='caption' sx={{
-                    color:'var(--link)',
-                    '&:hover':{
-                        
-                        textDecoration:'underline'
-
-                    },
-                    cursor:'pointer'
-                }}>American Eagle</Typography>
-            <Typography variant='caption' sx={{
-                    color:'var(--link)',
-                    '&:hover':{
-                        
-                        textDecoration:'underline'
-
-                    },
-                    cursor:'pointer'
-                }}>&gt;</Typography>
-            <Typography variant='caption' sx={{
-                    color:'var(--link)',
-                    '&:hover':{
-                        
-                        textDecoration:'underline'
-
-                    },
-                    cursor:'pointer'
-                }}>Men</Typography>
-        </Stack>
-        <Grid container>
-                <Grid item md={2}>
-                    <Box>
-                        <CategoryComponent type={type} />
-                    </Box>
-                </Grid>
-                <Grid item md={10}>
-                    <Box>
-                        <Typography variant='h4' textTransform={'uppercase'}>{type}</Typography>
-                        <SizeComponent filters={filters} type={type} onHandleFilters={handleChangeFilters} />
-                        <Stack direction={'row'} spacing={4} marginY={2}>
-                            <SizeFilter filter={sortBy} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
-                            <Filter filterType='color' onHandleFilters={handleChangeFilters} filter={colors} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
-                            <Filter filterType='size' onHandleFilters={handleChangeFilters} filter={sizes} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
-                            <Filter filterType='price' onHandleFilters={handleChangeFilters} filter={prices} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
-                        </Stack>
-                        {
-                            filters.length > 0 && 
-                            <Stack marginY={2} direction='row' justifyContent={'space-between'} alignItems={'center'}>
-                                <Stack direction={'row'} spacing={2}>
-                                    <Typography whiteSpace={'nowrap'}>Selected Filters </Typography>
-                                    <Stack direction={'row'} flexWrap={'wrap'} rowGap={1} spacing={1}>
-                                    {filters.map((filter,index)=>
-                                        <Button 
-                                            onClick={()=>handleChangeFilters(filter)}
-                                            key={index}
-                                            disableRipple 
-                                            sx={
-                                                {   
-                                                    border:'1px solid var(--border)',
-                                                    fontSize:'12px',
-                                                    borderRadius:0,
-                                                    color:'var(--black)', 
-                                                    paddingY: 0,
-                                                    paddingX:0.5,
-                                                    justifyContent:'space-evenly',
-                                                    '&:hover': { 
-                                                        background: 'none'
-                                                    } 
-                                                }
-                                            }
-                                        >
-                                            <Typography marginRight={0.2} fontSize='12px'>{filter.label} </Typography>
-                                        
-                                            <ClearIcon sx={{color:'var(--black)',fontSize:'12px'}}  />
-                                        </Button>
-                                    )}
-                                    </Stack>
-                                </Stack>
-                                <Button 
-                                    
-                                    onClick={()=>setFilters([])}
-                                    sx={{
-                                        textDecoration:'underline',
-                                        cursor:'pointer',
-                                        color:'var(--link)',
-                                        whiteSpace:'nowrap',
-                                        '&:hover': { 
-                                            background: 'none',
-                                            textDecoration:'underline',
-                                        } 
-                                    }}
-                                    >Clear Filters</Button>
+        <Container fixed >
+            <Stack direction='row' spacing={2} overflow={'auto'}>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={newArrivals} alt="New Arrivals" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>New Arrivals</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={tees} alt="Graphic tees" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Graphic tees</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={polos} alt="Polos" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Polos</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={shirts} alt="Shirts & Flannels" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Shirts & Flannels</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={hoodies} alt="Hoodies & SweatShirts" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Hoodies & SweatShirts</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={jeans} alt="Jeans" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Jeans</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={joggers} alt="Joggers & Sweatpants" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Joggers & Sweatpants</Typography>
+                </Box>
+                <Box  sx={{ cursor:'pointer' }}>
+                    <img width={'120px'}  src={under} alt="Underwear" />
+                    <Typography textAlign={'center'} width='100px' whiteSpace={'wrap'} lineHeight={1.2} variant='body2'>Underwear</Typography>
+                </Box>
+            </Stack>
+            <Stack spacing={1} marginY={1} direction='row' justifyContent={'flex-start'} alignItems={'center'}>
+                <Typography variant='caption' sx={{
+                        color:'var(--link)',
+                        '&:hover':{
                             
+                            textDecoration:'underline'
+
+                        },
+                        cursor:'pointer'
+                    }}>Home </Typography>
+                <Typography variant='caption' sx={{
+                        color:'var(--link)',
+                        '&:hover':{
+                            
+                            textDecoration:'underline'
+
+                        },
+                        cursor:'pointer'
+                    }}>&gt;</Typography>
+                <Typography variant='caption' sx={{
+                        color:'var(--link)',
+                        '&:hover':{
+                            
+                            textDecoration:'underline'
+
+                        },
+                        cursor:'pointer'
+                    }}>American Eagle</Typography>
+                <Typography variant='caption' sx={{
+                        color:'var(--link)',
+                        '&:hover':{
+                            
+                            textDecoration:'underline'
+
+                        },
+                        cursor:'pointer'
+                    }}>&gt;</Typography>
+                <Typography variant='caption' sx={{
+                        color:'var(--link)',
+                        '&:hover':{
+                            
+                            textDecoration:'underline'
+
+                        },
+                        cursor:'pointer'
+                    }}>Men</Typography>
+            </Stack>
+            <Grid columnSpacing={7} container>
+                    <Grid item md={3} display={{xs:'none',md:'block'}} >
+                        <Box>
+                            <CategoryComponent type={type} />
+                        </Box>
+                    </Grid>
+                    <Grid item md={9} sm={12}>
+                        <Box>
+                            <Typography variant='h4' textTransform={'uppercase'}>{type}</Typography>
+                            <SizeComponent filters={filters} type={type} onHandleFilters={handleChangeFilters} />
+                            <Stack direction={'row'} spacing={4} marginY={2}>
+                                <SizeFilter filter={sortBy} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
+                                <Filter filterType='color' onHandleFilters={handleChangeFilters} filter={colors} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
+                                <Filter filterType='size' onHandleFilters={handleChangeFilters} filter={sizes} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
+                                <Filter filterType='price' onHandleFilters={handleChangeFilters} filter={prices} activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
                             </Stack>
-                        }
-                        
-                    </Box>
-                    <ProductListGrid />
-                </Grid>
-        </Grid>
-        </Stack>
+                            {
+                                filters.length > 0 && 
+                                <Stack marginY={2} direction='row' justifyContent={'space-between'} alignItems={'center'}>
+                                    <Stack direction={'row'} spacing={2}>
+                                        <Typography whiteSpace={'nowrap'}>Selected Filters </Typography>
+                                        <Stack direction={'row'} flexWrap={'wrap'} rowGap={1} spacing={1}>
+                                        {filters.map((filter,index)=>
+                                            <Button 
+                                                onClick={()=>handleChangeFilters(filter)}
+                                                key={index}
+                                                disableRipple 
+                                                sx={
+                                                    {   
+                                                        border:'1px solid var(--border)',
+                                                        fontSize:'12px',
+                                                        borderRadius:0,
+                                                        color:'var(--black)', 
+                                                        paddingY: 0,
+                                                        paddingX:0.5,
+                                                        justifyContent:'space-evenly',
+                                                        '&:hover': { 
+                                                            background: 'none'
+                                                        } 
+                                                    }
+                                                }
+                                            >
+                                                <Typography marginRight={0.2} fontSize='12px'>{filter.label} </Typography>
+                                            
+                                                <ClearIcon sx={{color:'var(--black)',fontSize:'12px'}}  />
+                                            </Button>
+                                        )}
+                                        </Stack>
+                                    </Stack>
+                                    <Button 
+                                        
+                                        onClick={()=>setFilters([])}
+                                        sx={{
+                                            textDecoration:'underline',
+                                            cursor:'pointer',
+                                            color:'var(--link)',
+                                            whiteSpace:'nowrap',
+                                            '&:hover': { 
+                                                background: 'none',
+                                                textDecoration:'underline',
+                                            } 
+                                        }}
+                                        >Clear Filters</Button>
+                                
+                                </Stack>
+                            }
+                            
+                        </Box>
+                        <ProductListGrid />
+                    </Grid>
+            </Grid>
+        </Container>
     )
 }
 
